a099502b030f7aee1a0c1af270e82c38d007ef33
h766hajimetakagi <hajime.takagi.h766@dirbato.co.jp>
2024-04-23 15:12:49 +0900

    UpdateServlet.javaを作成

---
+++ message_board/src/controllers/UpdateServlet.java
@@ -0,0 +1,56 @@
+package controllers;
+
+import java.io.IOException;
+import java.sql.Timestamp;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import models.Message;
+import utils.DButil;
+
+@WebServlet("/Update")
+public class UpdateServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public UpdateServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+	        throws ServletException, IOException {
+	    String _token = request.getParameter("_token");
+        if (_token != null && _token.equals(request.getSession().getId())) {
+            var em = DButil.createEntityManager();
+
+            // セッションスコープからメッセージのIDを取得して
+            // 該当のIDのメッセージ1件のみをデータベースから取得
+            var m = em.find(Message.class, (Integer) (request.getSession().getAttribute("message_id")));
+
+            // フォームの内容を各フィールドに上書き
+            var title = request.getParameter("title");
+            m.setTitle(title);
+
+            var content = request.getParameter("content");
+            m.setContent(content);
+
+            var currentTime = new Timestamp(System.currentTimeMillis());
+            m.setUpdated_at(currentTime); // 更新日時のみ上書き
+
+            // データベースを更新
+            em.getTransaction().begin();
+            em.getTransaction().commit();
+            em.close();
+
+            // セッションスコープ上の不要になったデータを削除
+            request.getSession().removeAttribute("message_id");
+
+            // indexページへリダイレクト
+            response.sendRedirect(request.getContextPath() + "/index");
+        }
+
+	}
+
+}
